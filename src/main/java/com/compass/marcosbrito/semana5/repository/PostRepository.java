dab28d9c63a1e347893ae6b081b34ffedcac75c1
Marcos Brito <marcosbryto92@gmail.com>
2024-12-05 16:31:15 -0300

    Buscar posts contendo um dado string no título. Usando o Query Metodh

--- src/main/java/com/compass/marcosbrito/semana5/repository/PostRepository.java
+++ src/main/java/com/compass/marcosbrito/semana5/repository/PostRepository.java
@@ -3,8 +3,23 @@ package com.compass.marcosbrito.semana5.repository;
 import com.compass.marcosbrito.semana5.domain.Post;
 import com.compass.marcosbrito.semana5.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.sql.Date;
+import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post,String> {
+
+    /*
+     Usando Query procurar post com um determinado texto
+     */
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
+
+    List<Post> findByTitleContainingIgnoreCase(String text);
+
+    @Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
+    List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
