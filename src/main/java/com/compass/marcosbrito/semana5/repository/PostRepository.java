d61b908185bae35ab9770dbeb7982831ce3fe1f4
Marcos Brito <marcosbryto92@gmail.com>
2024-12-05 16:52:24 -0300

    "Buscar posts contendo um dado string em qualquer lugar (no título, corpo ou comentários) e em um dado intervalo de datas"

--- src/main/java/com/compass/marcosbrito/semana5/repository/PostRepository.java
+++ src/main/java/com/compass/marcosbrito/semana5/repository/PostRepository.java
@@ -26,6 +26,8 @@ public interface PostRepository extends MongoRepository<Post,String> {
 
     List<Post> findByTitleContainingIgnoreCase(String text);
 
+
+    //Tenho que verificar 3 condioes Ands. Sendo 2 para verificar as datas e 2 para verificar se pertente a um dos campos Post, Title ou Comment
     @Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")
     List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
